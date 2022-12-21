62fe8ad247527dfadfeae54d2bea0e16e970d5b8
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-21 22:54:04 +0100

    Update Buttons

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -25,6 +25,13 @@ export default {
             <FooterCharacter :info="element" />
         </div>
     </div>
+    <div class="buttons">
+        <button v-for="(link, index) in store.menuNav" @click.prevent :key="index">
+            <a :href="link.url">
+                {{ link.text }}
+            </a>
+        </button>
+    </div>
 </template>
 
 <style lang="scss" scoped>
@@ -35,5 +42,25 @@ export default {
     @include center;
     background-color: $primary;
     width: 100%;
+}
+
+.buttons {
+    width: 100%;
+    height: 100px;
+    background-color: $primary;
+    @include center;
+
+    button {
+        margin: 20px 140px;
+        width: 200px;
+        height: 40px;
+        background-color: $primary;
+        border: 1px solid $fourth;
+        color: $fourth;
+    }
+
+    a {
+        text-decoration: none;
+    }
 }
 </style>

0f55a3f98c6500f1bed49ca79ed7fdf45e1dac5b
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-21 16:36:30 +0100

    Update AppFooter.vue

---
+++ src/components/AppFooter.vue
@@ -0,0 +1,41 @@
+<script>
+
+// Importo il componente "SingleCharacter"
+import FooterCharacter from './FooterCharacter.vue'
+
+import { store } from '../store.js';
+
+export default {
+    name: "AppFooter",
+    components: {
+        FooterCharacter,
+    },
+    data() {
+        return {
+            store,
+        }
+    }
+
+}
+</script>
+
+<template>
+    <div class="container">
+        <div v-for="element in store.contentFooter" :key="element">
+            <FooterCharacter :info="element" />
+        </div>
+    </div>
+
+</template>
+
+<style lang="scss" scoped>
+@use 'src/style/partials/_variables' as*;
+@use 'src/style/partials/_mixins' as*;
+
+.container {
+    @include around;
+    background-color: $primary;
+    width: 100%;
+    min-height: 500px;
+}
+</style>

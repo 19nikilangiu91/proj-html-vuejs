4a2e1e2924ba4f90556e34e078cee75e38d8752a
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-21 20:47:35 +0100

    Update Content

--- src/store.js
+++ src/store.js
@@ -5,25 +5,33 @@ export const store = reactive({
     // Header Content
     headerTitle: "Barber Shop",
     underTitle: "The Pinnacle of Male Grooming",
+
+    // Main Top Content
+    upMainTitle: "AWARD WINNING",
+    mainTitle: "Pro Barbers",
+
+    // Footer Content
     contentFooter: [
         {
             img: "images/avadabarbers_footer_shop_image-300x234.png",
             text: "Shop Our Range Now",
+            url: "#",
 
         },
         {
             img: "images/avadabarbers-logo-x1.png",
-            // textone: "Avada Barbers",
-            // texttwo: "23 New York Street",
-            // textthree: "New York City",
-            // textfour: "info@yourwebsite.com",
-            // textfive: "+1(555) 555-1212",
-            // textsix: "Follow Us",
+            textOne: "Avada Barbers",
+            textTwo: "23 New York Street",
+            textThree: "New York City",
+            textFour: "info@yourwebsite.com",
+            textFive: "+1(555) 555-1212",
+            textSix: "Follow Us",
         },
         {
             img: "images/avadabarbers_footer_appointment_image-300x234.png",
             text: "Book Your Appointment",
+            url: "#",
 
         }
-    ]
+    ],
 });

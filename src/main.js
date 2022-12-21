2c4b305fd1f726c5e0ddb24b84561bc83ec13b95
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-21 13:17:53 +0100

    Update Bootstrap + FontAwesome

--- src/main.js
+++ src/main.js
@@ -1,5 +1,16 @@
 import { createApp } from 'vue'
-import './style.css'
+// import './style.css'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+// Bootstrap Install
+import "bootstrap"
+
+// FontAwesome
+import { library } from '@fortawesome/fontawesome-svg-core'
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+
+/* add icons to the library */
+library.add(faCartShopping)
+
+createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')

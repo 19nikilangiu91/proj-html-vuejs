c55396a25cea6087826552a31d8e0771449c73b4
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-22 15:03:41 +0100

    Imported Icons

--- src/main.js
+++ src/main.js
@@ -9,8 +9,9 @@ import "bootstrap"
 import { library } from '@fortawesome/fontawesome-svg-core'
 import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
 import { faCartShopping } from '@fortawesome/free-solid-svg-icons'
+import { faFacebookF, faInstagram, faTwitter, faYoutube } from '@fortawesome/free-brands-svg-icons'
 
 /* add icons to the library */
-library.add(faCartShopping)
+library.add(faCartShopping, faFacebookF, faTwitter, faYoutube, faInstagram)
 
 createApp(App).component('font-awesome-icon', FontAwesomeIcon).mount('#app')

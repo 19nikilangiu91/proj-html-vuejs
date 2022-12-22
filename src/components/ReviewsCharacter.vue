40057ee49b707faead5e928066cebf6872a271a6
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-22 12:10:14 +0100

    Created ReviewsCharacter

---
+++ src/components/ReviewsCharacter.vue
@@ -0,0 +1,47 @@
+<script>
+
+export default {
+    name: "ReviewsCharacter",
+    props: ["info"],
+}
+
+</script>
+
+<template>
+    <!-- Template di Single Card -->
+    <div>
+        <p>{{ info.text }}</p>
+        <img :src="info.img" alt="image">
+        <h6>{{ info.name }}</h6>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use 'src/style/partials/_variables' as*;
+@use 'src/style/partials/_mixins' as*;
+
+div {
+    @include column;
+    width: 100%;
+    margin: 20px 0 50px 0;
+
+    img {
+        height: 100px;
+        margin-top: 30px;
+        border-radius: 50%;
+    }
+
+    h6 {
+        margin: 10px 0;
+        text-align: center;
+        font-size: 15px;
+        color: $fourth;
+    }
+
+    p {
+        text-align: center;
+        font-size: 18px;
+        padding: 0 20px;
+    }
+}
+</style>

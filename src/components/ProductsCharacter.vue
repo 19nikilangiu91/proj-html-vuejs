bba06bc0979288442ccc7c5dd8e671550ffc1d00
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-22 10:43:54 +0100

    Created ProductsCharacter.vue

---
+++ src/components/ProductsCharacter.vue
@@ -0,0 +1,44 @@
+<script>
+
+export default {
+    name: "ProductsCharacter",
+    props: ["info"],
+}
+
+</script>
+
+<template>
+    <!-- Template di Single Card -->
+    <div>
+        <img :src="info.img" alt="image">
+        <h3>{{ info.title }}</h3>
+        <p>{{ info.text }}</p>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+@use 'src/style/partials/_variables' as*;
+@use 'src/style/partials/_mixins' as*;
+
+div {
+    width: 100%;
+
+    img {
+        width: 250px;
+    }
+
+    h3 {
+        margin: 10px 0;
+        text-align: center;
+        font-size: 25px;
+        color: $fourth;
+    }
+
+    p {
+        text-align: center;
+        font-size: 18px;
+        padding: 0 20px;
+        color: $fourth;
+    }
+}
+</style>

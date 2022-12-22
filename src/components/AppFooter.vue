65a033146bc9dc8d8c19135f03d5b80db573eecb
19nikilangiu91 <niki.langiu@gmail.com>
2022-12-22 22:59:35 +0100

    Update Ul

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -24,6 +24,13 @@ export default {
         <div v-for="element in store.contentFooter" :key="element">
             <FooterCharacter :info="element" />
         </div>
+        <ul class="specialUl">
+            <li><font-awesome-icon icon=" fa-brands fa-facebook-f" />
+            </li>
+            <li><font-awesome-icon icon="fa-brands fa-twitter" /></li>
+            <li><font-awesome-icon icon="fa-brands fa-youtube" /></li>
+            <li><font-awesome-icon icon="fa-brands fa-instagram" /></li>
+        </ul>
     </div>
     <div class="buttons">
         <button v-for="(link, index) in store.menuNav" @click.prevent :key="index">
@@ -31,12 +38,6 @@ export default {
                 {{ link.text }}
             </a>
         </button>
-        <ul class="specialUl">
-            <li><font-awesome-icon icon="fa-brands fa-facebook-f" /></li>
-            <li><font-awesome-icon icon="fa-brands fa-twitter" /></li>
-            <li><font-awesome-icon icon="fa-brands fa-youtube" /></li>
-            <li><font-awesome-icon icon="fa-brands fa-instagram" /></li>
-        </ul>
     </div>
 </template>
 
@@ -58,27 +59,29 @@ export default {
 
     button {
         position: relative;
-        margin: 20px 140px;
-        width: 200px;
-        height: 40px;
+        width: 150px;
+        height: 50px;
+        margin: 0 170px;
         background-color: $primary;
         border: 1px solid $fourth;
         color: $fourth;
     }
 
     a {
+        font-size: 12px;
         text-decoration: none;
     }
 }
 
 .specialUl {
     @include center;
+    align-self: flex-end;
     position: absolute;
     list-style-type: none;
 
     li {
         margin: 10px;
-        color: $third;
+        color: $fifth;
     }
 }
 </style>
